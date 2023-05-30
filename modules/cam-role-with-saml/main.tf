965b9acba6b0465e22505790d2674a944e1aa62d
ericwhzhao <ericwhzhao@tencent.com>
2023-05-30 17:26:27 +0800

    format some files

--- modules/cam-role-with-saml/main.tf
+++ modules/cam-role-with-saml/main.tf
@@ -9,18 +9,18 @@ resource "tencentcloud_cam_saml_provider" "saml_provider_basic" {
 resource "tencentcloud_cam_role" "role" {
   count = var.create_role ? 1 : 0
 
-  document = var.role_document
-  name = var.role_name
+  document      = var.role_document
+  name          = var.role_name
   console_login = var.console_login
-  description = var.role_description
-  tags = var.role_tags
+  description   = var.role_description
+  tags          = var.role_tags
 }
 
 resource "tencentcloud_cam_policy" "policy" {
   count = var.create_policy ? 1 : 0
 
-  name = var.policy_name
-  document = var.policy_document
+  name        = var.policy_name
+  document    = var.policy_document
   description = var.policy_description
 }
 
--- modules/cam-role-with-saml/variables.tf
+++ modules/cam-role-with-saml/variables.tf
@@ -1,37 +1,37 @@
 variable "create_saml" {
   description = "Whether to create a saml provider."
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "create_role" {
   description = "Whether to create a role."
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "create_policy" {
   description = "Whether to create a policy."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "saml_name" {
   description = "The name of CAM SAML provider."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "meta_data" {
   description = "The meta data document of the CAM SAML provider."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "saml_description" {
   description = "The description of the CAM SAML provider."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "role_document" {
@@ -40,44 +40,44 @@ variable "role_document" {
   1. The elements in json claimed supporting two types as string and array only support type array; 
   2. Terraform does not support the root syntax, when appears, it must be replaced with the uin it stands for.
   EOF
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "role_name" {
   description = "Name of CAM role."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "console_login" {
   description = "Indicates whether the CAM role can login or not."
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "role_description" {
   description = "Description of the CAM role."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "role_tags" {
   description = "A list of tags used to associate different resources."
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "policy_name" {
   description = "Name of CAM policy."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "policy_description" {
   description = "Description of the CAM policy."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "policy_document" {
@@ -86,12 +86,12 @@ variable "policy_document" {
   1. The elements in JSON claimed supporting two types as string and array only support type array; 
   2. Terraform does not support the root syntax, when it appears, it must be replaced with the uin it stands for.
   EOF
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "policy_id" {
   description = "ID of the policy."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
