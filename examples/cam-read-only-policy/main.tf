10f469e81100ad239b649f450afcc5b2bfe4329f
bobklxiao <bobklxiao@tencent.com>
2023-06-05 15:01:23 +0800

    feat:add cam-read-only-policy module and example

---
+++ examples/cam-read-only-policy/main.tf
@@ -0,0 +1,38 @@
+#########################################
+# CAM read only policy
+#########################################
+module "read_only_policy" {
+  source = "../../modules/cam-read-only-policy"
+  name = "bob-tf-policy"
+  description = "My example policy"
+  allowed_services = ["cos","cvm"]
+  additional_policy = [ {
+                                   action: [
+                                       "monitor:Get*",
+                                       "monitor:Describe*"
+                                   ],
+                                   effect: "allow",
+                                   resource: [
+                                       "*"
+                                   ]
+                               },
+                               {
+                                   action: [
+                                       "cam:ListUsersForGroup",
+                                       "cam:ListGroups",
+                                       "cam:GetGroup",
+                                       "cam:ListAttachedRolePolicies"
+                                   ],
+                                   effect: "allow",
+                                   resource: [
+                                       "*"
+                                   ]
+                               }]
+}
+
+
+module "read_only_policy_optional" {
+  source = "../../modules/cam-read-only-policy"
+
+  create_policy = false
+}
