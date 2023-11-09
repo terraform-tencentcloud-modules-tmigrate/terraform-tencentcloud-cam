7bb050d95ca6b3ca07fec66bbc3c84e7d3d75348
madongdong <donaldma@tencent.com>
2023-11-10 01:05:15 +0800

    add user-policy module

---
+++ examples/cam-user-policy/outputs.tf
@@ -0,0 +1,18 @@
+output "user_ids" {
+  description = "The user ids"
+  value = module.cam_group_user_policy.user_ids
+}
+
+output "user_passwords" {
+  description = "The user passwords"
+  value = module.cam_group_user_policy.user_passwords
+}
+
+output "policiy_ids" {
+  value = module.cam_group_user_policy.policy_ids
+  description = "The policy ids"
+}
+
+output "access_keys" {
+  value = module.cam_group_user_policy.access_keys
+}
